4d1be00f16f3c39b7db5793e341992a2f3a64c9c
Karan Hudia <karanhudia@gmail.com>
2016-06-20 17:47:20 +0530

    Fixed top navigation bars according to UI

--- public/js/_scripts.js
+++ public/js/_scripts.js
@@ -1,4 +1,4 @@
 /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+    document.getElementsByClassName("menu-item-nav")[0].classList.toggle("responsive");
 }
